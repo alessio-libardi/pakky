a8bf59fcb2478c51cf11d7d8055735fdc5f495e7
Alessio Libardi <alessio@wonderflow.co>
2021-05-10 22:16:47 +0200

    feat(pakky): features

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,13 @@ module.exports = {
   darkMode: "media",
   purge: ["./src/**/*.html"],
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      black: "black",
-      white: "white",
-      primary: {
-        light: "#059669",
-        dark: "#34D399",
-      },
-      gray: {
-        light: "#fafafa",
-        dark: "#111111",
-      },
-    },
     extend: {
+      colors: {
+        gray: {
+          light: "#fafafa",
+          dark: "#111111",
+        },
+      },
       animation: {
         "hero-background": "fade-in 1s ease-in 250ms forwards",
         "hero-title": "fade-in 250ms ease-in 1s forwards",
@@ -42,6 +34,75 @@ module.exports = {
           },
         },
       },
+      typography: (theme) => ({
+        dark: {
+          css: [
+            {
+              color: theme("colors.gray.400"),
+              '[class~="lead"]': {
+                color: theme("colors.gray.300"),
+              },
+              a: {
+                color: theme("colors.white"),
+              },
+              strong: {
+                color: theme("colors.white"),
+              },
+              "ol > li::before": {
+                color: theme("colors.gray.400"),
+              },
+              "ul > li::before": {
+                backgroundColor: theme("colors.gray.600"),
+              },
+              hr: {
+                borderColor: theme("colors.gray.200"),
+              },
+              blockquote: {
+                color: theme("colors.gray.200"),
+                borderLeftColor: theme("colors.gray.600"),
+              },
+              h1: {
+                color: theme("colors.white"),
+              },
+              h2: {
+                color: theme("colors.white"),
+              },
+              h3: {
+                color: theme("colors.white"),
+              },
+              h4: {
+                color: theme("colors.white"),
+              },
+              "figure figcaption": {
+                color: theme("colors.gray.400"),
+              },
+              code: {
+                color: theme("colors.white"),
+              },
+              "a code": {
+                color: theme("colors.white"),
+              },
+              pre: {
+                color: theme("colors.gray.200"),
+                backgroundColor: theme("colors.gray.800"),
+              },
+              thead: {
+                color: theme("colors.white"),
+                borderBottomColor: theme("colors.gray.400"),
+              },
+              "tbody tr": {
+                borderBottomColor: theme("colors.gray.600"),
+              },
+            },
+          ],
+        },
+      }),
     },
   },
+  variants: {
+    extend: {
+      typography: ["dark"],
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+const { resolve } = require("path");
+
 export default {
   root: "src",
   base: "/pakky/",
   server: {
     open: true,
+  },
+  build: {
+    emptyOutDir: true,
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+      },
+    },
   },
 };
