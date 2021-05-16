32c0dc996a376f3c9eb8ffec1338553245b041a6
Alessio Libardi <alessio@wonderflow.co>
2021-05-16 19:27:13 +0200

    chore(pakky): testing different config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 
 export default defineConfig({
   root: "src",
-  base: process.env.ENV === "development" ? "/" : "/pakky/",
+  base: "/pakky/",
   server: {
     open: true,
   },
